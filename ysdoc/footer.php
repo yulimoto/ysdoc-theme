4ff5e9c6781fa7a7471abcf16cc925f2dc76d71e
Юлий Димитров <websemiotic@fourthgnossienne>
2014-02-02 14:50:37 +0200

    Create links and README.md

--- ysdoc/footer.php
+++ ysdoc/footer.php
@@ -15,7 +15,7 @@
 			<?php do_action( 'ysdoc_credits' ); ?>
 			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'ysdoc' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'ysdoc' ), 'ysdoc', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'ysdoc' ), 'ysdoc', '<a href="https://github.com/yulimoto/ysdoc-theme" rel="designer">ysdoc-theme</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #content -->
