d204c14b3c4e83a4148487accc39f8197e80f258
Юлий Димитров <websemiotic@fourthgnossienne>
2014-02-02 15:04:41 +0200

    Restructure folders

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 			<?php do_action( 'ysdoc_credits' ); ?>
 			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'ysdoc' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'ysdoc' ), 'ysdoc', '<a href="https://github.com/yulimoto/ysdoc-theme" rel="designer">ysdoc-theme</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'ysdoc' ), 'ysdoc', '<a href="https://github.com/yulimoto/ysdoc-theme" rel="designer">yulimoto\'s github</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #content -->
--- ysdoc/README.md
+++
@@ -1,7 +0,0 @@
-
-=== ysdoc ===
-This is theme for Wordpress 3 based on underscores  for my linux administration blog. 
-http://yulimoto.bgadmin.eu 
-
-
-
